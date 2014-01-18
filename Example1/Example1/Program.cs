01d8c5662651de7807d5373bef86fd3e18aa00a3
Roman Pushkin <ro@code42.ru>
2014-01-18 12:10:58 +0400

    Adding console example

---
+++ Example1/Example1/Program.cs
@@ -0,0 +1,18 @@
+﻿using RomanPushkin.BetterRobokassa;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string redirectUrl = Robokassa.GetRedirectUrl(1000, 1);
+            Console.WriteLine(redirectUrl);
+        }
+    }
+}
