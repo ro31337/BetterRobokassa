999d35a91cc01055d92b206d498888c829eed15a
Roman Pushkin <ro@code42.ru>
2014-01-25 18:49:32 +0400

    Example 2: Adding Payment Controller Stub

---
+++ Example2/Controllers/PaymentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Example2.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public ActionResult Index()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
